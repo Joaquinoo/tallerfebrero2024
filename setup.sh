4730bf126e880fb49d5b12bc33775cdd591753a8
Joaquín Laguzzi <joaquinopersonal14@gmail.com>
2024-02-27 19:10:36 -0300

    Modificación en initial.yml y creación de setup.sh
    Modificamos initial.yml para copiar las claves públicas hacia
    el usuario de ansible dentro del playbook.
    
    Generamos setup.sh para configuración inicial del entorno.

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+ssh-keygen
+
+echo -n "Ingrese el nombre de usuario del administrador de los servidores de destino: "
+read user
+
+echo -e "---------------------------------------------------------------------------------------"
+echo -e "**Copiado de clave pública a servidores de destino utilizando $user**"
+echo -e ""
+echo -n "Ingrese la IP de uno de los servidores de destino (ingrese 0 para continuar el setup): "
+read ip
+
+while [ "$ip" != "0" ]
+do
+  ssh-copy-id $user@$ip
+  echo -n "Ingrese la IP del servidor de destino (ingrese 0 para continuar el setup): "
+  read ip
+done
+
+echo -e "---------------------------------------------------------------------------------------"
+echo -e "**Instalando colecciones necesarias**"
+ansible-galaxy install -r requirements.yml
+
+echo -e "---------------------------------------------------------------------------------------"
+echo -e "**Ejecutando initial.yml para configuración inicial de los servidores remotos**"
+ansible-playbook -i inventories/hosts playbooks/initial.yml
